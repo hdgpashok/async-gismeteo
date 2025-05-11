6799800108ed191876ba018587236264629126a5
Pavel <bobrov_71@list.ru>
2025-05-11 17:12:12 +0300

    added logging to get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -1,17 +1,21 @@
 import json
 from aiohttp import ClientSession, web
+from logger import logger
 
 
 async def get_weather(city):
     async with ClientSession() as session:
         url = f'http://api.openweathermap.org/data/2.5/weather'
         params = {'q': city, 'APPID': 'e4f66a04bfaf25cb376d6062a8b1bb94'}
+        await logger.info(f'Получил запрос на город {city}')
 
-        async with session.get(url= url, params= params) as responce:
+        async with session.get(url=url, params=params) as responce:
             weather_json = await responce.json()
+
             try:
+                logger.info(f'Успешно передал информацию о городе {city}')
                 return weather_json['weather'][0]['main']
+
             except KeyError:
+                logger.error(f'Ошибка данных для города {city}')
                 return 'Нет данных'
-
-
