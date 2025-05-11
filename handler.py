27086e30590af84a9bb933f1edefe9c1990b6e44
Pavel <bobrov_71@list.ru>
2025-05-11 17:11:51 +0300

    edited handler.py

--- handler.py
+++ handler.py
@@ -1,4 +1,4 @@
-import asyncio
+from logger import logger
 import json
 from aiohttp import ClientSession, web
 from get_weather import get_weather
@@ -6,9 +6,7 @@ from get_weather import get_weather
 
 async def handler(request):
     city = request.rel_url.query['city']
-
     weather = await get_weather(city)
-
     result = {'city': city, 'weather': weather}
 
     return web.Response(text=json.dumps(result, ensure_ascii=False))
